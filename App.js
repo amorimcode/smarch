3ab0b8358dcadae0cfb301c07d23ba1674041a66
Bruno <bruno.amorim032@gmail.com>
2020-12-24 15:58:37 -0300

    a

--- App.js
+++ App.js
@@ -1,29 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/Header'
+import Header from './src/components/Header';
+import axios from 'axios';
 
 
-export default class App() extends React.Component {
+export default class App extends React.Component {
   renderList() {
-    const names [
-      'Eddie Van Hallen',
-      'Deleo',
-      'Thiago Bit',
-      'Slash'
-    ]
+    // const textElements = names.map(name => { 
+    //   return <Text key={ name }>{ name }</Text>
+
+    // } )
+    axios.get('https://randomuser.me/api/?nat-br&results=5')
+         .then(response => {
+           console.log(response.data);
+         })
+
+
+    // return textElements
+
   }
 
 
 
-
-  return (
-    <View>
-      <Header title="Pessoas!" />
-      { this.renderList() }
-    </View>
-  );
+  render() {
+    return (
+      <View>
+        <Header title="Pessoas!" />
+        { this.renderList()}
+      </View>
+    );
+  }
 }
+
 
 
 
